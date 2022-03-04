8338e590aa33c9c55bfe5d98a9e959194da8c1a4
assebc <claudioafbessa@gmail.com>
2022-03-04 18:02:21 +0000

    exercise 2 and 3

--- 2ano/POO (Programming Oriented Objects)/fichas/Ficha02.java
+++ 2ano/POO (Programming Oriented Objects)/fichas/Ficha02.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -64,12 +65,141 @@ public class Ficha02{
 	}
 
 
-	public static void Exercicio2(){}
-	public static void Exercicio3(){}
+	public static void Exercicio2(){
+		int [][] notasTurma = new int [5][5]; // i alunos j cadeiras
+
+		System.out.println("Nota");
+		Scanner input = new Scanner(System.in);
+		int base = input.nextInt();
+		for(int i = 0;i<5;i++){
+			for(int j = 0;j<5;j++){
+				notasTurma[i][j] = base;
+				
+			}
+			System.out.println(Arrays.toString(notasTurma[i]));
+			base++;
+		}
+
+		
+		System.out.println("Determina UC entre 0 e menor que 5");
+		int uc = input.nextInt();
+		int med = 0;
+		for(int j = 0;j<5;j++) med += notasTurma[uc][j];
+		med /= 5;
+		System.out.print("Media da uc " +uc); // concat 1
+		System.out.println(" é "+med); // concat 2
+
+		System.out.println("Determina aluno entre 0 e menor que 5");
+		int aluno = input.nextInt();
+		int med_aluno = 0;
+		for(int j = 0;j<5;j++) med_aluno += notasTurma[aluno][j];
+		med_aluno /= 5;
+		System.out.print("Media do aluno " +aluno); // concat 1
+		System.out.println(" é "+med_aluno); // concat 2
+
+		
+
+		int max = 0, ucB = 0, alunoB = 0;
+		for(int i = 0;i<5;i++){
+			for(int j = 0;j<5;j++){
+				if (notasTurma[i][j] > max){
+					max = notasTurma[i][j];
+					alunoB = i;
+					ucB = j;
+				}
+			}
+		}
+		System.out.print("A maior nota é a "+ ucB);
+		System.out.print(" do "+ alunoB);
+		System.out.println(" e é "+max);
+
+		int small = 20, ucS = 0, alunoS = 0;
+		for(int i = 0;i<5;i++){
+			for(int j = 0;j<5;j++){
+				if (notasTurma[i][j] < small){
+					small = notasTurma[i][j];
+					alunoS = i;
+					ucS = j;
+				}
+			}
+		}
+		System.out.print("A menor nota é a "+ ucS);
+		System.out.print(" do "+ alunoS);
+		System.out.println(" e é "+small);
+		
+
+		int [] aux = new int [25];
+		System.out.println("Acima de?");
+		int limit = input.nextInt();
+		int ocorr = 0, rind = 0;
+		for(int i = 0;i<5;i++){
+			for(int j = 0;j<5;j++){
+				if(notasTurma[i][j]>limit){
+					ocorr++;
+					aux[rind++] = notasTurma[i][j];
+				}
+			}
+		}
+		int [] result = new int [ocorr];
+		for(int i = 0;i<ocorr;i++) result[i] = aux[i];
+		System.out.println(Arrays.toString(result));
+		
+		float mediaUc = 0, ucs = 0;
+		int ind = 0;
+		for(int i = 0;i<5;i++){
+			for(int j = 0;j<5;j++){
+				ucs += notasTurma[i][j];
+			}
+			if(ucs/5 > mediaUc){
+				mediaUc = ucs/5;
+				ind = i;
+			}
+			ucs = 0;
+		}
+		System.out.print("Maior média é " +ind);
+		System.out.println(" com "+mediaUc);
+
+
+		input.close();
+	}
+
+	public static void swap(int [] array, int i, int j){
+		int tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+
+	public static void Exercicio3(){
+
+		int [] array = {10, 32, 21, 54, 13, 16}; // 10 13 16 21 32 54
+		System.out.println(Arrays.toString(array));
+		for(int i = 0;i<array.length-1;i++){
+			for(int j = i+1;j<array.length;j++){
+				if(array[i]>array[j]) swap(array,j,i);
+			}
+		}
+		System.out.println(Arrays.toString(array));
+		Scanner input = new Scanner(System.in);
+		System.out.println("Diga 2 indices");
+		int a = input.nextInt();
+		int b = input.nextInt();
+
+		int [] sub_array = new int [b-a+1];
+		int indsub = 0;
+		for(int i = a;i<b+1;i++){
+			sub_array[indsub++] = array[i];
+		}
+
+		System.out.println(Arrays.toString(sub_array));
+		input.close();
+
+	}
+
 	public static void Exercicio4(){}
+
 	public static void Exercicio5(){}
+
 	public static void Exercicio6(){}
-	public static void Exercicio7(){}
 
 
 	public static void main(String [] args){
@@ -98,10 +228,6 @@ public class Ficha02{
 
 			case 6 :
 			Exercicio6();
-			break;
-
-			case 7 :
-			Exercicio7();
 			break;
 
 
