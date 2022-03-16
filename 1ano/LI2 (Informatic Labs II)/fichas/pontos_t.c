9106fc72c46d5527fec2308b71c23eda14afaee4
assebc <claudioafbessa@gmail.com>
2022-03-16 20:19:14 +0000

    helping fabio

---
+++ 1ano/LI2 (Informatic Labs II)/fichas/pontos_t.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void triangulos(int x, int y){
+	int i, count = 0, sum=0;
+	
+	for(int j = x;j<=y;j++){
+		for(i=0;i<=j; i++){
+			sum = sum+i;
+			if(sum == j) {
+				count++;
+	   			break;
+	  		}
+		}
+		sum = 0;
+	}
+	printf("%d ", count);
+
+}
+
+void primos(int x, int y){
+	int count = 0;
+  	int a,b;
+    
+  	for (a=2;a<=y;a++){
+      	int c=0;
+      	for (b=1;b<=a;b++){
+          	if (a%b==0) c++;
+       }
+       if (c==2) count++;
+    }
+
+	printf("%d\n", count);
+}
+
+int main(){
+
+	int x = 0, y = 0;
+	printf("Diga 2 vars\n");
+	scanf("%d %d",&x,&y);
+	int a = x; int b = y;
+	triangulos(x,y); 
+	primos(a,b);
+
+	return 0;
+}
