a2db2113861308022d0fdf155e2509141d051f63
assebc <claudioafbessa@gmail.com>
2022-03-23 11:57:50 +0000

    stack done~

--- 2ano/POO (Oriented Objects Programming)/fichas/Ficha04.java
+++ 2ano/POO (Oriented Objects Programming)/fichas/Ficha04.java
@@ -1,1 +1,31 @@
-import
+import java.util.ArrayList;
+
+public class Ficha04{
+
+    public class Stack {
+        private ArrayList<String> STACK = new ArrayList<>();
+
+        public boolean empty() {
+            return (STACK.isEmpty());
+        }
+
+        public String top() {
+            if (empty()) return null;
+            return STACK.get(STACK.size() - 1);
+        }
+
+        public void push(String a) {
+            STACK.add(a);
+        }
+
+        public void pop() {
+            STACK.remove(STACK.size() - 1);
+        }
+
+        public int length() {
+            return (STACK.size());
+        }
+    }
+
+    public class
+}
