10fd2a97937f0ad1ac067618c96df016b6e224f0
assebc <claudioafbessa@gmail.com>
2022-11-30 13:21:50 +0000

    :heavy_plus_sign: no immutable concatlist but working concurrently

--- 3rd_year/SD (Distributed Systems)/tasks/task7/src/Contact.java
+++ 3rd_year/SD (Distributed Systems)/tasks/task7/src/Contact.java
@@ -22,13 +22,43 @@ class Contact {
     public int age() { return age; }
     public long phoneNumber() { return phoneNumber; }
     public String company() { return company; }
-    public List<String> emails() { return new ArrayList(emails); }
+    public List<String> emails() { return new ArrayList<>(emails);}
 
-    // @TODO
-    public void serialize (DataOutputStream out) throws IOException { }
+    public void serialize (DataOutputStream out) throws IOException {
+        out.writeUTF(this.name);
+        out.writeInt(this.age);
+        out.writeLong(this.phoneNumber);
 
-    // @TODO
-    public static Contact deserialize (DataInputStream in) throws IOException { }
+        if (company!=null) {
+            out.writeBoolean(true);
+            out.writeUTF(this.company);
+        }else {
+            out.writeBoolean(false);
+        }
+
+        out.writeInt(this.emails.size());
+        for (String email : this.emails) {
+            out.writeUTF(email);
+        }
+
+    }
+    public static Contact deserialize (DataInputStream in) throws IOException {
+        String name = in.readUTF();
+        int age = in.readInt();
+        long phoneNumber = in.readLong();
+
+        String company = null;
+        if (in.readBoolean()) {
+            company = in.readUTF();
+        }
+
+        ArrayList<String> emails = new ArrayList<>();
+        int emailsize = in.readInt();
+        for (int i = 0;i<emailsize;i++)
+            emails.add(in.readUTF());
+
+        return new Contact(name,age,phoneNumber,company,emails);
+    }
 
     public String toString () {
         StringBuilder builder = new StringBuilder();
--- 3rd_year/SD (Distributed Systems)/tasks/task7/src/Server.java
+++ 3rd_year/SD (Distributed Systems)/tasks/task7/src/Server.java
@@ -1,29 +1,33 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
+import java.io.DataInputStream;
 import static java.util.Arrays.asList;
+
 
 class ContactManager {
     private HashMap<String, Contact> contacts;
+    private ReentrantLock lck = new ReentrantLock();
 
     public ContactManager() {
         contacts = new HashMap<>();
         // example pre-population
-        contacts.update(new Contact("John", 20, 253123321, null, asList("john@mail.com")));
-        contacts.update(new Contact("Alice", 30, 253987654, "CompanyInc.", asList("alice.personal@mail.com", "alice.business@mail.com")));
-        contacts.update(new Contact("Bob", 40, 253123456, "Comp.Ld", asList("bob@mail.com", "bob.work@mail.com")));
+        this.update(new Contact("John", 20, 253123321, null, asList("john@mail.com")));
+        this.update(new Contact("Alice", 30, 253987654, "CompanyInc.", asList("alice.personal@mail.com", "alice.business@mail.com")));
+        this.update(new Contact("Bob", 40, 253123456, "Comp.Ld", asList("bob@mail.com", "bob.work@mail.com")));
     }
-
-
-    // @TODO
     public void update(Contact c) {
+        try {
+            lck.lock();
+            contacts.put(c.name(), c);
+        }
+        finally {
+            lck.unlock();
+        }
     }
 
-    // @TODO
-    public ContactList getContacts() { }
 }
 
 class ServerWorker implements Runnable {
@@ -35,9 +39,21 @@ class ServerWorker implements Runnable {
         this.manager = manager;
     }
 
-    // @TODO
     @Override
-    public void run() { }
+    public void run() {
+        try {
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+
+            while (true) {
+                Contact c = Contact.deserialize(in);
+                System.out.println(c);
+                this.manager.update(c);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
@@ -45,13 +61,14 @@ class ServerWorker implements Runnable {
 public class Server {
 
     public static void main (String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(12345);
-        ContactManager manager = new ContactManager();
+        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+            ContactManager manager = new ContactManager();
 
-        while (true) {
-            Socket socket = serverSocket.accept();
-            Thread worker = new Thread(new ServerWorker(socket, manager));
-            worker.start();
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Thread worker = new Thread(new ServerWorker(socket, manager));
+                worker.start();
+            }
         }
     }
 
