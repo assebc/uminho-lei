fbf07bcbbee73f82fe8a6d3c8c86d73415a51234
assebc <claudioafbessa@gmail.com>
2022-11-30 10:58:11 +0000

    :wrench: fixed connection, waiting demultiplexer

--- 3rd_year/SD (Distributed Systems)/tasks/task8/src/ServerWithWorkers.java
+++ 3rd_year/SD (Distributed Systems)/tasks/task8/src/ServerWithWorkers.java
@@ -5,38 +5,39 @@ public class ServerWithWorkers {
     final static int WORKERS_PER_CONNECTION = 3;
 
     public static void main(String[] args) throws Exception {
-        ServerSocket ss = new ServerSocket(12345);
-
-        while(true) {
-            Socket s = ss.accept();
-            FramedConnection c = new FramedConnection(s);
-
-            Runnable worker = () -> {
-                try (c) {
-                    for (;;) {
-                        Frame frame = c.receive();
-                        int tag = frame.tag;
-                        String data = new String(frame.data);
-                        if (frame.tag == 0)
-                            System.out.println("Got one-way: " + data);
-                        else if (frame.tag % 2 == 1) {
-                            System.out.println("Replying to: " + data);
-                            c.send(frame.tag, data.toUpperCase().getBytes());
-                        } else {
-                            for (int i = 0; i < data.length(); ++i) {
-                                String str = data.substring(i, i+1);
-                                System.out.println("Streaming: " + str);
-                                c.send(tag, str.getBytes());
-                                Thread.sleep(100);
+        try (ServerSocket ss = new ServerSocket(12345)) {
+            while(true) {
+                Socket s = ss.accept();
+                try (FramedConnection c = new FramedConnection(s)) {
+                    
+                    Runnable worker = () -> {
+                        try {
+                            while(true) {
+                                TaggedConnection.Frame frame = c.receive();
+                                int tag = frame.tag;
+                                String data = new String(frame.data);
+                                if (frame.tag == 0)
+                                    System.out.println("Got one-way: " + data);
+                                else if (frame.tag % 2 == 1) {
+                                    System.out.println("Replying to: " + data);
+                                    c.send(frame.tag, data.toUpperCase().getBytes());
+                                } else {
+                                    for (int i = 0; i < data.length(); ++i) {
+                                        String str = data.substring(i, i+1);
+                                        System.out.println("Streaming: " + str);
+                                        c.send(tag, str.getBytes());
+                                        Thread.sleep(100);
+                                    }
+                                    c.send(tag, new byte[0]);
+                                }
                             }
-                            c.send(tag, new byte[0]);
-                        }
-                    }
-                } catch (Exception ignored) { }
-            };
+                        } catch (Exception ignored) { }
+                    };
 
-            for (int i = 0; i < WORKERS_PER_CONNECTION; ++i)
-                new Thread(worker).start();
+                    for (int i = 0; i < WORKERS_PER_CONNECTION; ++i)
+                        new Thread(worker).start();
+                }
+            }
         }
 
     }
