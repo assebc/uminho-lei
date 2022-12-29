d967af2402cff2e55677d66e372bfcc26fae5be5
assebc <claudioafbessa@gmail.com>
2022-12-29 18:09:40 +0000

    :heavy_plus_sign: exams statements

--- 3rd_year/SD (Distributed Systems)/exams/20220119/interface.java
+++
@@ -1,9 +0,0 @@
-interface Votacao{
-
-    boolean verifica(int identidade);
-    int esperaPorCabine();
-    void vota(int escolha);
-    void desocupaCabine(int i);
-
-    int vencedor(); // apenas para a alínea de Valorização
-}
