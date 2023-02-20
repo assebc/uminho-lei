46d0241670c3e5b9d583a811f9c7ec2b10dfbb2e
assebc <claudioafbessa@gmail.com>
2023-02-20 10:26:44 +0000

    feat: task2

---
+++ 3rd_year/PL (Language Programming)/tasks/task2/task2.py
@@ -0,0 +1,16 @@
+import re
+
+def exercise1():
+  t1 = r"(a+b)*"
+  t2 = r"a* + "
+  t3 = r"(ab)+a*"
+  t4 = r"a(a+b)*b"
+  t5 = r"a+b*"
+
+def exercise2():
+  t1 = r"a*+b*+c*"
+  t2 = r"a*(ba)*b*"
+  t3 = r"a*(b++c+)a*"
+
+def exercise3():
+  er = r"(ab)+c(d + fe+ b)*a"
