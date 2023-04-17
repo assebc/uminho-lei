31e5c5aea56021472f48ba64ee84c67ef84a865e
assebc <claudioafbessa@gmail.com>
2023-04-17 09:45:19 +0100

    feat: task6 calc_yacc done

---
+++ 3rd_year/PL (Language Programming)/tasks/task6/calc_lex.py
@@ -0,0 +1,31 @@
+import ply.lex as lex
+
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN'
+)
+
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore = ' \n\t'
+
+def t_error(t):
+    print(f"Carácter ilegal {t.value[0]}")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
